4cf38dbb4c0ecd1f313b94922991cc84be226a0a
Alfie Atkinson <alf.atkinson13@gmail.com>
2025-02-08 19:08:06 +0000

    Add Navbar component for site navigation

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Navbar(): React.JSX.Element {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/about'>About</a>
+        </li>
+        <li>
+          <a href='/projects'>Projects</a>
+        </li>
+        <li>
+          <a href='/contact'>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
