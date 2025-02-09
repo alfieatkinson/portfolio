37750bd8a91bba880fb93cad4d4aab340b3a735c
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-09 10:29:16 +0000

    Add social media constants for contact and profiles

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+const social = {
+  contactMail: 'contact@alfieatkinson.dev',
+  careerMail: 'career@alfieatkinson.dev',
+  linkedin: 'https://www.linkedin.com/in/alfieatkinson/',
+  github: 'https://github.com/alfieatkinson'
+}
+
+export default {
+  social
+}
