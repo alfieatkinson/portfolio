e778549d2f74a83cfddfd5767fbcf105567f05a5
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-09 11:12:43 +0000

    remove

--- src/components/common/reusable/button/Button.types.ts
+++
@@ -1,8 +0,0 @@
-import ComponentProps from '@/types/component'
-
-export default interface ButtonProps extends ComponentProps {
-  onClick?: () => void
-  icon?: React.ReactNode
-  screenReaderText?: string
-  active?: boolean
-}
