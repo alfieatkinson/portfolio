70cf6c81e9acbbd1139f1dba85d7763af07a76a5
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-11 11:23:40 +0000

    Add InlineLink component for enhanced link styling with sliding underline effect

---
+++ src/components/common/reusable/InlineLink.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { lazy } from 'react'
+import clsx from 'clsx'
+import InlineLinkProps from '@/types/components/InlineLinkProps'
+
+const SlidingInUnderline = lazy(() => import('@/components/common/reusable/SlidingInUnderline'))
+
+export default function InlineLink({ href, children }: InlineLinkProps): React.JSX.Element {
+  return (
+    <a
+      href={href}
+      target='_blank'
+      rel='noreferrer'
+      className={clsx(
+        'cursor-pointer',
+        'font-semibold duration-300',
+        'text-primary-dark hover:text-primary-lighter-dark',
+        'dark:text-primary-light dark:hover:text-primary-lighter'
+      )}
+    >
+      <SlidingInUnderline>{children}</SlidingInUnderline>
+    </a>
+  )
+}
