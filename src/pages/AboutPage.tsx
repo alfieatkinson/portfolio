84d5fc701c22cc6624331f26e925b7fce9ed0b88
alfieatkinson <alf.atkinson13@gmail.com>
2025-02-11 12:07:33 +0000

    Fetch and display content from markdown file in AboutPage component

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import { lazy, Suspense } from 'react'
+import { lazy, Suspense, useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
+import about from '@/_data/about.md'
 import Preloader from '@/components/common/Preloader'
 
 const PageWrapper = lazy(() => import('@/components/layout/PageWrapper'))
 const Navbar = lazy(() => import('@/components/layout/Navbar'))
+const About = lazy(() => import('@/components/sections/About'))
 const Footer = lazy(() => import('@/components/layout/Footer'))
 
 export default function AboutPage(): React.JSX.Element {
+  const [content, setContent] = useState<string>('')
+
+  useEffect((): void => {
+    fetch(about as RequestInfo)
+      .then((response: Response): Promise<string> => response.text())
+      .then((text: string): void => {
+        setContent(text)
+        localStorage.about = text
+      })
+      .catch((): void => setContent('Failed to load content. Please reload the page!'))
+  }, [content])
+
   return (
     <>
       <Helmet>
@@ -20,6 +34,7 @@ export default function AboutPage(): React.JSX.Element {
       <Suspense fallback={<Preloader />}>
         <PageWrapper>
           <Navbar />
+          <About children={content} />
           <Footer />
         </PageWrapper>
       </Suspense>
