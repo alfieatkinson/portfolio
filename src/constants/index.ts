eae35473d64f6f6a1daa1bc3b690ee8bc3159923
alfieatkinson <alf.atkinson13@gmail.com>
2025-03-05 17:55:11 +0000

    Add resume file link to constants and update GitHub URL format

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,10 +2,15 @@ const social = {
   contactMail: 'contact@alfieatkinson.dev',
   careerMail: 'career@alfieatkinson.dev',
   linkedin: 'https://www.linkedin.com/in/alfieatkinson/',
-  github: 'https://github.com/alfieatkinson',
+  github: 'alfieatkinson',
   discord: 'https://discord.com/users/407254472105000981'
 }
 
+const files = {
+  resume: 'https://raw.githubusercontent.com/alfieatkinson/Curriculum-Vitae/main/main.pdf'
+}
+
 export default {
-  social
+  social,
+  files
 }
