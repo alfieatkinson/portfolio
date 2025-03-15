27100001cc6d7cde3a7cd070d4979c4b8a075404
alfieatkinson <alf.atkinson13@gmail.com>
2025-03-15 11:42:21 +0000

    Add initial page component with static parameters and main CSS import

---
+++ src/app/[[...slug]]/page.tsx
@@ -0,0 +1,9 @@
+import '@/assets/css/main.css'
+
+export function generateStaticParams() {
+  return [{ slug: [''] }];
+}
+
+export default function Page() {
+  return '...'; // You'll update this later
+}
